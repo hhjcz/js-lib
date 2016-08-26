655ffe96d5fbe0f5497b48e48eab686dcbf25270
Jan Haering <hhj@centrum.cz>
2016-08-26 10:13:23 +0200

    feat: publish defaults

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/** Created by hhj on 8/26/16. */
+import debounce from './debounce'
+import longestSubstring from './longestSubstring'
+import redux from './redux'
+
+export {
+  debounce,
+  longestSubstring,
+  redux,
+}
---
+++ src/redux/index.js
@@ -0,0 +1,10 @@
+/** Created by hhj on 8/26/16. */
+import createMapStateToProps from './createMapStateToProps'
+import createMapDispatchToProps from './createMapDispatchToProps'
+import myMiddleware from './myMiddleware'
+
+export {
+  createMapStateToProps,
+  createMapDispatchToProps,
+  myMiddleware,
+}
